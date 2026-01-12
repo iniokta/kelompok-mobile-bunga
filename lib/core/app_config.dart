269567c990cd88ccd3ce8c30b5005b3b20cc5ce5
Nabila Aulia <nabilaauliaramadhani.teknokrat@gmail.com>
2026-01-12 17:34:40 +0700

    fix bug app_config.dart

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   // GANTI INI dengan Mock Base URL dari Apidog lo
-  static const String baseUrl = "PASTE_MOCK_BASE_URL_DI_SINI";
+  static const String baseUrl = "PASTE_MOCK_BASE_URL_DISINI";
 }
